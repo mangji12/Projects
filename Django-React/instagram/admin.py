a4b44ba8a5a9db5de8014a3f3e854b65e5c78afb
mangji12 <dhckddls12@naver.com>
2023-05-07 10:33:55 +0900

    info update

--- Django-React/instagram/admin.py
+++ Django-React/instagram/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from instagram.models import Post
+
+
 # Register your models here.
+@admin.register(Post)
+class Post(admin.ModelAdmin):
+  list_display = ['author','title','content']
--- Django-React/instagram/models.py
+++ Django-React/instagram/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+  author = models.ForeignKey(User,on_delete=models.CASCADE)
+  title = models.CharField(max_length=20)
+  content = models.TextField(blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return f'작성자 : {self.author}'
