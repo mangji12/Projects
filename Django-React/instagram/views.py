dd44e527f2628b1ecb1c667b301e1613aea64758
mangji12 <dhckddls12@naver.com>
2023-05-09 00:21:36 +0900

    Implemented messages

--- Django-React/instagram/views.py
+++ Django-React/instagram/views.py
@@ -5,30 +5,35 @@ from django.views.generic import ListView, DetailView
 
 from instagram.forms import PostForm
 from instagram.models import Post
+from django.contrib import messages
 
 # Create your views here.
 # CBV (post_list)
-# @login_required(Post) # 장식자 예 2
+# @login_required # 장식자 예 2
 post_list = ListView.as_view(model=Post,template_name='post_list.html',queryset=Post.objects.all()) # 쿼리셋 옵션에 쿼리셋을 등록하면 모델명(소문자)_list로 변수가 전달됨
 # FBV (post_list)
 # @login_required # 장식자 예 1
 # def post_list(request):
-#   qs = Post.objects.all(pk=User.pk)
+#   qs = Post.objects.all()
 #   q = request.GET.get('q','')
 #   if q:
 #     qs = qs.filter(title__icontains=q) # 모델의 title 속성 중 q 인자에 삽입된 값이 포함된 값을 __icontains 인자로서 검색을 구현한다.
+#   messages.info(request, '새 글이 등록되었습니다.') # 상속의 부모 템플릿에 거의 적용.
 #   return render(request,'post_list.html',{'qs' : qs,'q' : q})
 
 @login_required
-def post_new(request):
+def post_new(request,pk):
+  # post = get_object_or_404(Post, pk=pk)
   if request.method == 'POST':
-    form = PostForm(request.POST,request.FILES) # 폼에서 데이터를 날리면 해당 구문으로 데이터를 받는다. (속성 대문자 주의)
+    form = PostForm(request.POST,request.FILES, instance=post) # 폼에서 데이터를 날리면 해당 구문으로 데이터를 받는다. (속성 대문자 주의)
     if form.is_valid():
       post = form.save() # commit=True가 default. 객체를 생성하고 save()를 해야 실제 DB에 값이 저장이 된다.
+      # messages.add_message(request, messages,SUCCESS, '새 글이 등록되었습니다.')
+      messages.info(request, '새 글이 등록되었습니다.') # 혹은 shortcut 형태
       return redirect(post) # 모델에 get_absolute_url이 구현되어 있으면 이동.
   else:
     form = PostForm() # 파일 로딩이 안됐으므로, 유효성 검사에 적합한 값을 넣도록 다시 되돌린다.
-  return render(request,'post_form.html',{'form' : form})
+  return render(request,'post_form.html',{'form' : form,'post':None})
 
 
 # FBV (edit)
@@ -36,21 +41,22 @@ def post_new(request):
 def post_edit(request, pk):
   post = get_object_or_404(Post,pk=pk)
 
-  # 작성자 체크 팁
+  # 작성자 체크
   if post.author != request.user:
     messages.error(request, '작성자만 수정할 수 있습니다.')
     return redirect(post)
 
   if request.method == 'POST':
-    form = PostForm(request.POST,request.FILES, instance=post) # 폼에서 데이터를 날리면 해당 구문으로 데이터를 받는다. (속성 대문자 주의) instance 속성으로 기존의 정보를 가져온다.
+    form = PostForm(request.POST,request.FILES, instance=post) # 폼에서 데이터를 날리면 해당 구문으로 데이터를 받는다. (속성 대문자 주의) ,instance 속성으로 post 쿼리셋도 가져온다.
     if form.is_valid():
       post = form.save(commit=False) # commit=True가 default. 객체를 생성하고 save()를 해야 실제 DB에 값이 저장이 된다.
       post.author = request.user # 현재 로그인 User Instance
       post = form.save()
+      messages.info(request,'포스팅을 수정하였습니다.')
       return redirect(post) # 모델에 get_absolute_url이 구현되어 있으면 이동.
   else:
     form = PostForm(instance=post) # 파일 로딩이 안됐으므로, 유효성 검사에 적합한 값을 넣도록 다시 되돌린다.
-  return render(request,'post_form.html',{'form' : form})
+  return render(request,'post_form.html',{'form' : form,'post':post})
 
 # FBV (detail)
 # def post_detail(request, id):
